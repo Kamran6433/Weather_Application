0c7e1cbec6d5a962d1ff3ecbfe6fbda962fc79eb
kamran6433 <kamran_12@icloud.com>
2022-12-28 12:38:47 +0000

    Made huge changes to the project by using the Vue CLI to create a simple template to aid me.

--- components/WeatherApplication.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <DisplayData></DisplayData>
-    </div>
-</template>
---
+++ my-weather-application/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <h1>My Weather Application?</h1>
+        <WeatherApplication></WeatherApplication>
+    </div>
+</template>
+
+<script>
+import WeatherApplication from './components/WeatherApplication.vue';
+
+export default {
+    name: 'App',
+    components: {
+      WeatherApplication
+    }
+}
+
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
