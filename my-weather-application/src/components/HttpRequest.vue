75f77f8c19cca5214892980d8be5b58cb637e866
kamran6433 <kamran_12@icloud.com>
2023-01-03 16:12:05 +0000

    Improving the weather application using another API.

--- my-weather-application/src/components/HttpRequest.vue
+++ my-weather-application/src/components/HttpRequest.vue
@@ -10,7 +10,13 @@
         <div>
             <h3>Weather data will be displayed here</h3>
             <hr/>
+            <h1>First API call</h1>
+            <p id="api"></p>
             <p id="data"></p>
+            <h1>Location</h1>
+            <p id="location-data"></p>
+            <h1>Temperature(Celcius)</h1>
+            <p id="temperature-data"></p>
         </div>
     </div>
 </template>
@@ -32,9 +38,32 @@
                 var latitude = this.latitude;
                 var longitude = this.longitude;
                 var parameters = 'hourly=temperature_2m';
-                var url = 'https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&'+parameters;
+                var url1 = 'https://timezoneapi.io/api/address/?Hacker+Way+1+Menlo+Park+California&token=BOqSQMWnZEml';
+                var url2 = 'https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&'+parameters;
 
-                fetch(url)
+                fetch(url1)
+                    .then(response => {
+                        if (!response.ok) {
+                            return Promise.reject(response.statusText);
+                        }
+                        return response.json();
+                    })
+                    .then(good_response => {
+                        var JSONDataStore = JSON.stringify(good_response, null, 4);
+                        localStorage.setItem("./assets/LocationData.JSON", JSONDataStore);
+
+                        var text = localStorage.getItem("./assets/LocationData.JSON");
+                        var str = JSON.parse(text);
+                        console.log(str);
+                        document.getElementById('api').innerHTML = str
+                    })
+                    .catch(error => {
+                        document.getElementById('api').innerHTML = `API call Failed.`;
+                        console.error('There was an error in first API call!', error);
+                    });
+
+
+                fetch(url2)
                     .then(response => {
                         if (!response.ok) {
                             return Promise.reject(response.statusText);
@@ -48,13 +77,9 @@
                         var text = localStorage.getItem("./assets/WeatherData.JSON");
                         var str = JSON.parse(text);
                         console.log(str);
-                        // let objectData = "";
-                        // for (const x in str) {
-                        //     objectData += str[x] + ", ";
-                        // }
-                        var temperature_2m = str['hourly']
-                        document.getElementById('data').innerHTML = temperature_2m['temperature_2m'];
-                        // document.getElementById('data').innerHTML = JSONDataStore;
+                        var temperature_2m = str['hourly'];
+                        document.getElementById('temperature-data').innerHTML = temperature_2m['temperature_2m'];
+                        document.getElementById('location-data').innerHTML = temperature_2m['temperature_2m'];
                     })
                     .catch(error => {
                         document.getElementById('data').innerHTML = `Please enter correct co-ordinates`;
