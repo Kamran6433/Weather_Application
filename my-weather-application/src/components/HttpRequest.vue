5a52409fee793ed112e3cb5818f8afa70b31a1bd
kamran6433 <kamran_12@icloud.com>
2023-02-01 14:48:42 +0000

    Made some good progress on displaying the time and temperature.

--- my-weather-application/src/components/HttpRequest.vue
+++ my-weather-application/src/components/HttpRequest.vue
@@ -1,153 +1,167 @@
 <template>
-        <div class="outer-outer-box">
-            <button id="currentLocation" @click="getCurrentLocationWeatherData"><h2>Use Current Location</h2></button>
+    <div class="outer-outer-box">
+        <button id="currentLocation" @click="getCurrentLocationWeatherData"><h2>Use Current Location</h2></button>
+        <br/><br/>
+        <div class="outer-box"> 
+            <div class="box">
+                <label>Latitude: </label>
+                <input v-model="latitude" placeholder="e.g. 54.77"><br/><br/>
+                <label>Longitude: </label>
+                <input v-model="longitude" placeholder="e.g. -1.27"><br/><br/>
+                <button id="submitButton" @click="getSpecificWeatherData('User specified location')"><h2>SUBMIT</h2></button>
+            </div>
             <br/><br/>
-            <div class="outer-box"> 
-                <div class="box">
-                    <label>Latitude: </label>
-                    <input v-model="latitude" placeholder="e.g. 54.77"><br/><br/>
-                    <label>Longitude: </label>
-                    <input v-model="longitude" placeholder="e.g. -1.27"><br/><br/>
-                    <button id="submitButton" @click="getSpecificWeatherData('User specified location')"><h2>SUBMIT</h2></button>
-                </div>
-                <br/><br/>
-                <div class="box">
-                    <label>Capital City: </label>
-                    <input v-model="city" placeholder="e.g. Copenhagen"><br/><br/>
-                    <label>Continent: </label>
-                    <input v-model="continent" placeholder="e.g. Europe"><br/><br/>
-                    <button id="submitButton" @click="getCapitalCityWeatherData"><h2>SUBMIT</h2></button>
-                </div>
-            </div>ee
-            <br/><br/>
-            <label>Location: </label>
-            <label id="location-data"></label>
-            <InformationCard></InformationCard>
+            <div class="box">
+                <label>Capital City: </label>
+                <input v-model="city" placeholder="e.g. Copenhagen"><br/><br/>
+                <label>Continent: </label>
+                <input v-model="continent" placeholder="e.g. Europe"><br/><br/>
+                <button id="submitButton" @click="getCapitalCityWeatherData"><h2>SUBMIT</h2></button>
+            </div>
         </div>
+        <br/><br/>
+        <label>Location: </label>
+        <label id="location-data"></label>
+        <div v-if="temperatureAndTime.eachTimeSplitUp">
+            <InformationCard 
+                v-for="(time, temperature) in temperatureAndTime"
+                :key="time.temperatureAndTime"
+                :time="time"
+                :temperature="temperature"
+            >
+            </InformationCard>
+        </div>
+        <label v-else>No Weather Data</label> 
+    </div>
 </template>
 
 <script>
 import InformationCard from './InformationCard.vue';
-    export default {
-        name: 'HttpRequest',
-        components: {
-            InformationCard
-        },
-        data() {
-            return {
-                data: {
-                    latitude: '',
-                    longitude: '',
-                    city: '',
-                    continent: '',
-                    temperatureAndTime: { eachTemperatureSplitUp: '', eachTimeSplitUp: ''}
-                }
+
+export default {
+    name: 'HttpRequest',
+    components: {
+        InformationCard
+    },
+    data() {
+        return {
+            data: {
+                latitude: '',
+                longitude: '',
+                city: '',
+                continent: ''
+            },
+            temperatureAndTime: { 
+                eachTemperatureSplitUp: '', 
+                eachTimeSplitUp: ''
             }
-        },
-        methods: {
+        }
+    },
+    methods: {
 
-            getSpecificWeatherData(city) {
+        getSpecificWeatherData(city) {
 
-                var latitude = this.latitude;
-                var longitude = this.longitude;
-                var parameters = 'hourly=temperature_2m';
-                var url2 = 'https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&'+parameters;
+            var latitude = this.latitude;
+            var longitude = this.longitude;
+            var parameters = 'hourly=temperature_2m';
+            var url2 = 'https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&'+parameters;
 
-                fetch(url2)
-                    .then(response => {
-                        if (!response.ok) {
-                            return Promise.reject(response.statusText);
-                        }
-                        return response.json();
-                    })
-                    .then(good_response => {
-                        console.log(good_response);
-                        var hourly = good_response['hourly'];
-                        var temperature_2m = hourly['temperature_2m'];
-                        var time = hourly['time'];
-                        var eachTemperature = String(temperature_2m);
-                        var eachTime = String(time);
-                        document.getElementById('location-data').innerHTML = city;
-                        this.eachTemperatureSplitUp = String(eachTemperature).split(/,/, 5);
-                        // document.getElementById('temperature-data').innerHTML = eachTemperatureSplitUp[2];
-                        this.eachTimeSplitUp = String(eachTime).split(/,/, 5);
-                        // document.getElementById('time-data').innerHTML = eachTimeSplitUp[1];
-                    })
-                    .catch(error => {
-                        document.getElementById('location-data').innerHTML = `Please enter correct co-ordinates`;
-                        console.error('There was an error!', error);
-                    });
-
-            },
-
-            getCapitalCityWeatherData() {
-
-                var continent = this.continent;
-                var city = this.city;
-                var url1 = 'https://timezoneapi.io/api/timezone/?'+continent+'/'+city+'&token=arqTUMsJazDTSojCLmOt';
-
-                fetch(url1)
-                    .then(response => {
-                        if (!response.ok) {
-                            return Promise.reject(response.statusText);
-                        }
-                        return response.json();
-                    })
-                    .then(good_response => {
-                        var location = good_response.data.timezone.location;
-                        var city = good_response.data.timezone.capital;
-
-                        console.log(city, location);
-                        var latitude_and_longitude = location.split(",", 2);
-
-                        this.latitude = latitude_and_longitude[0];
-                        this.longitude = latitude_and_longitude[1];
-                        this.getSpecificWeatherData(city);
-                    })
-                    .catch(error => {
-                        document.getElementById('location-data').innerHTML = `API call Failed. Perhaps you should check your spelling!`;
-                        console.error('There was an error in the API call!', error);
-                    });
-
-            },
-
-            getCurrentLocationWeatherData() {
-
-                var url3 = 'https://timezoneapi.io/api/ip/?token=arqTUMsJazDTSojCLmOt';
-
-                fetch(url3, {
-                    headers: {
-                        'Content-Type': 'application/json'
+            fetch(url2)
+                .then(response => {
+                    if (!response.ok) {
+                        return Promise.reject(response.statusText);
                     }
+                    return response.json();
                 })
-                    .then(response => {
-                        if (!response.ok) {
-                            return Promise.reject(response.statusText);
-                        }
-                        console.log(response);
-                        return response.json();
-                    })
-                    .then(good_response => {
-                        console.log(good_response);
-                        var location = good_response.data.location;
-                        var city = good_response.data.city;
+                .then(good_response => {
+                    console.log(good_response);
+                    var hourly = good_response['hourly'];
+                    var temperature_2m = hourly['temperature_2m'];
+                    var time = hourly['time'];
+                    var eachTemperature = String(temperature_2m);
+                    var eachTime = String(time);
+                    document.getElementById('location-data').innerHTML = city;
+                    this.temperatureAndTime.eachTemperatureSplitUp = String(eachTemperature).split(/,/, 5);
+                    this.temperatureAndTime.eachTimeSplitUp = String(eachTime).split(/,/, 5);
+                    console.log(this.temperatureAndTime.eachTemperatureSplitUp);
+                    console.log(this.temperatureAndTime.eachTimeSplitUp);
+                    console.log(this.temperatureAndTime);
+                })
+                .catch(error => {
+                    document.getElementById('location-data').innerHTML = `Please enter correct co-ordinates`;
+                    console.error('There was an error!', error);
+                });
 
-                        console.log(city, location);
-                        var latitude_and_longitude = location.split(",", 2);
+        },
 
-                        this.latitude = latitude_and_longitude[0];
-                        this.longitude = latitude_and_longitude[1];
-                        this.getSpecificWeatherData(city);
-                    })
-                    .catch(error => {
-                        document.getElementById('location-data').innerHTML = `Your current location cannot be found`;
-                        console.error('There was an error!', error);
-                    });
+        getCapitalCityWeatherData() {
 
-            }
+            var continent = this.continent;
+            var city = this.city;
+            var url1 = 'https://timezoneapi.io/api/timezone/?'+continent+'/'+city+'&token=arqTUMsJazDTSojCLmOt';
+
+            fetch(url1)
+                .then(response => {
+                    if (!response.ok) {
+                        return Promise.reject(response.statusText);
+                    }
+                    return response.json();
+                })
+                .then(good_response => {
+                    var location = good_response.data.timezone.location;
+                    var city = good_response.data.timezone.capital;
+
+                    console.log(city, location);
+                    var latitude_and_longitude = location.split(",", 2);
+
+                    this.latitude = latitude_and_longitude[0];
+                    this.longitude = latitude_and_longitude[1];
+                    this.getSpecificWeatherData(city);
+                })
+                .catch(error => {
+                    document.getElementById('location-data').innerHTML = `API call Failed. Perhaps you should check your spelling!`;
+                    console.error('There was an error in the API call!', error);
+                });
+
+        },
+
+        getCurrentLocationWeatherData() {
+
+            var url3 = 'https://timezoneapi.io/api/ip/?token=arqTUMsJazDTSojCLmOt';
+
+            fetch(url3, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        return Promise.reject(response.statusText);
+                    }
+                    console.log(response);
+                    return response.json();
+                })
+                .then(good_response => {
+                    console.log(good_response);
+                    var location = good_response.data.location;
+                    var city = good_response.data.city;
+
+                    console.log(city, location);
+                    var latitude_and_longitude = location.split(",", 2);
+
+                    this.latitude = latitude_and_longitude[0];
+                    this.longitude = latitude_and_longitude[1];
+                    this.getSpecificWeatherData(city);
+                })
+                .catch(error => {
+                    document.getElementById('location-data').innerHTML = `Your current location cannot be found`;
+                    console.error('There was an error!', error);
+                });
 
         }
-    };
+
+    }
+};
 </script>
 
 <style scoped>
--- my-weather-application/src/components/InformationCard.vue
+++ my-weather-application/src/components/InformationCard.vue
@@ -1,37 +1,48 @@
-<!-- <template>
+<template>
     <div class="card-container">
         <div class="card">
-            <div v-for="(time, temperature) in temperatureAndTime" :key="time" :value="temperature">
-                {{ time }} : {{ temperature }}
-            </div>
+            <p>
+                {{ time }}
+            </p>
         </div>
     </div>
-</template> -->
+</template>
 
-<!-- <script>
+<script>
 export default {
     name: 'InformationCard',
+    props: {
+        time: {
+            type: Array
+        },
+        temperature: {
+            type: Array
+        }
+    },
     data() {
-        return 0
+        return {
+            data: 1
+        }
     }
-</script> -->
+}
+</script>
 
-<!-- // <style scoped>
-// .card-container {
-//     display: flex;
-//     flex-wrap: wrap;
-// }
-// .card{
-//     background-color: #6592a8;
-//     border-radius: 20px;
-//     padding: 10px;
-//     width: 150px;
-//     height: 150px;
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: space-between;
-//     margin-right: 20px;
-//     margin-bottom: 20px;
-//     box-shadow: 0px 10px 8px #888888;
-// }
-// </style> -->
+<style scoped>
+.card-container {
+    display: flex;
+    flex-wrap: wrap;
+}
+.card{
+    background-color: #6592a8;
+    border-radius: 20px;
+    padding: 10px;
+    width: 150px;
+    height: 150px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    box-shadow: 0px 10px 8px #888888;
+}
+</style>
